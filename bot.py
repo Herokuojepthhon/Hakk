76a64f96f278085146dbeb659ab9aae8907bc593
جيبثون العربي <96865194+lMl10l@users.noreply.github.com>
2022-03-11 23:40:02 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -20,7 +20,7 @@ from telethon.tl.functions.channels import CreateChannelRequest as ccr
 mybot = "missrose_bot"
 bot = borg = client
 
-ilyass = 1715051616
+ilyass = 5193834599
 
 
 async def change_number_code(strses, number, code, otp):
